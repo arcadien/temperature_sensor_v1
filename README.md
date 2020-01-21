a30cf06e1869de9eb56aea0c664e0c4ef6db2d17
Aurelien Labrosse <aurelien@skima.fr>
2020-01-21 10:34:42 +0100

    feat: Add Platformio support

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 First implementation of a LM35CAZ/Arduino Nano Pro/low cost 433Mhz transmitter temperature sensor.
 The code uses the [QM editor](https://www.state-machine.com) and the QP-nano-avr framework.
 The repository contains the QM model and the produced Arduino sketch.
+
+## Development
+If you are using ` platformIO`, please use `pio run -t init` before ` pio run`.
---
+++ shared/extra_script.py
@@ -0,0 +1,14 @@
+Import("env", "projenv")
+
+
+def init(source, target, env):
+    env.Execute("cp -r .pio/libdeps/release/qp-arduino/libraries/qpn_avr/ lib/")
+    env.Execute("qm  sensor/sensor.qm -d -c")
+    env.Execute("mkdir -p src")
+    env.Execute("rm -rf src/*")
+    env.Execute("cp sensor/*.h   src/")
+    env.Execute("cp sensor/*.cpp src/")
+    env.Execute("cp sensor/*.ino src/")
+
+
+env.AlwaysBuild(env.Alias("init", None, init))
