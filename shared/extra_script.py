4d2329f38ada98548431c7d3d76ac676d229cf91
Aurelien Labrosse <aurelien@skima.fr>
2020-01-28 17:46:11 +0100

    feat: Preliminary support of ATmega32u4
    * Rewrite so that even less code is in the QM model
    * Various refactor

--- shared/extra_script.py
+++ shared/extra_script.py
@@ -1,14 +1,12 @@
 Import("env")
 
 
-def init(source, target, env):
+def generate(source, target, env):
     env.Execute("cp -r .pio/libdeps/release/qp-arduino/libraries/qpn_avr/ lib/")
     env.Execute("qm  sensor/sensor.qm -d -c")
     env.Execute("mkdir -p src")
-    env.Execute("rm -rf src/*")
-    env.Execute("cp sensor/*.h   src/")
-    env.Execute("cp sensor/*.cpp src/")
+    env.Execute("rm -rf src/*.ino")
     env.Execute("cp sensor/*.ino src/")
 
 
-env.AlwaysBuild(env.Alias("init", None, init))
+env.AlwaysBuild(env.Alias("generate", None, generate))
